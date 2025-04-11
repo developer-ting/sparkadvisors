c024b3e3674c5666d091f97a0b26b15140c7467d
NikhileshGurrapu13 <nikhilesh.gurrapu@ting.in>
2025-04-11 12:42:48 +0530

    title updated

--- risk_disclosures.php
+++ risk_disclosures.php
@@ -7,35 +7,36 @@
 <html class="no-js lt-ie9" lang=""> <![endif]-->
 <!--[if gt IE 8]><!-->
 <html class="no-js" lang="">
-<!--<![endif]-->
+    <!--<![endif]-->
 
-<head>
-    <?php include("includes/include_css.html") ?>
-    <?php include("includes/header_1.html") ?>
+    <head>
+        <?php include("includes/include_css.html") ?>
+        <?php include("includes/header_1.html") ?>
 
-</head>
+    </head>
 
-<body>
-    <?php include("includes/browser_upgrade.html") ?>
-    <!--  insert body content  -->
+    <body>
+        <?php include("includes/browser_upgrade.html") ?>
+        <!--  insert body content  -->
 
-    <section id="risk_disclosures" class="privacy_policy">
-        <section class="first_section">
-            <div class="container">
-                <div class="LogoImgBx">
-                    <img src="dist/img/risk_disclosures/SEBI_Logo.png" class="LogoImg img-responsive" alt="">
-                </div>
-                <h2>Annexure-I: Risk disclosures</h2>
-                <h4>RISK DISCLOSURES ON DERIVATIVES</h4>
-                <ul>
-                    <li>9 out of 10 individual traders in equity Futures and Options Segment, incurred net losses.</li>
-                    <li>On an average, loss makers registered net trading loss close to ₹ 50,000.</li>
-                    <li>Over and above the net trading losses incurred, loss makers expended an additional 28%
-                        of net trading losses as transaction costs.</li>
-                    <li>Those making net trading profits, incurred between 15% to 50% of such profits as
-                        transaction cost.</li>
-                </ul>
-                <!-- <p>
+        <section id="risk_disclosures" class="privacy_policy">
+            <section class="first_section">
+                <div class="container">
+                    <div class="LogoImgBx">
+                        <img src="dist/img/risk_disclosures/SEBI_Logo.png" class="LogoImg img-responsive" alt="">
+                    </div>
+                    <!-- <h2>Annexure-I: Risk disclosures</h2> -->
+                    <h2>RISK DISCLOSURES ON DERIVATIVES</h2>
+                    <ul>
+                        <li>9 out of 10 individual traders in equity Futures and Options Segment, incurred net losses.
+                        </li>
+                        <li>On an average, loss makers registered net trading loss close to ₹ 50,000.</li>
+                        <li>Over and above the net trading losses incurred, loss makers expended an additional 28%
+                            of net trading losses as transaction costs.</li>
+                        <li>Those making net trading profits, incurred between 15% to 50% of such profits as
+                            transaction cost.</li>
+                    </ul>
+                    <!-- <p>
                     9 out of 10 individual traders in equity Futures and Options Segment, incurred net losses.
                 </p>
                 <p>On an average, loss makers registered net trading loss close to ₹ 50,000.
@@ -44,23 +45,25 @@
                     of net trading losses as transaction costs. </p>
                 <p>Those making net trading profits, incurred between 15% to 50% of such profits as
                     transaction cost.</p> -->
-                <p>Source:</p>
-                <p>
-                    <a href="https://www.sebi.gov.in/reports-and-statistics/research/jan-2023/study-analysis-of-profit-and-loss-of-individual-traders-dealing-in-equity-fando-segment_67525.html"
-                        target="_blank">
-                        SEBI study dated January 25, 2023 on “Analysis of Profit and Loss of Individual Traders dealing
-                        in
-                        equity Futures and Options (F&O) Segment”, wherein Aggregate Level findings are based on annual
-                        Profit/Loss incurred by individual traders in equity F&O during FY 2021-22.
-                    </a>
-                </p>
-            </div>
+                    <p>Source:</p>
+                    <p>
+                        <a href="https://www.sebi.gov.in/reports-and-statistics/research/jan-2023/study-analysis-of-profit-and-loss-of-individual-traders-dealing-in-equity-fando-segment_67525.html"
+                            target="_blank">
+                            SEBI study dated January 25, 2023 on “Analysis of Profit and Loss of Individual Traders
+                            dealing
+                            in
+                            equity Futures and Options (F&O) Segment”, wherein Aggregate Level findings are based on
+                            annual
+                            Profit/Loss incurred by individual traders in equity F&O during FY 2021-22.
+                        </a>
+                    </p>
+                </div>
+            </section>
         </section>
-    </section>
 
-    <!--  end body content -->
-    <?php include("includes/include_js.html") ?>
-    <?php include("includes/footer.html") ?>
-</body>
+        <!--  end body content -->
+        <?php include("includes/include_js.html") ?>
+        <?php include("includes/footer.html") ?>
+    </body>
 
 </html>
