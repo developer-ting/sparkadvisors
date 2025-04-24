504c68cb76e9bec46ff5cb67c8966f5aea87c92c
NikhileshGurrapu13 <nikhilesh.gurrapu@ting.in>
2025-04-24 16:16:36 +0530

    logo updated on thank you page'

--- thank-you1.php
+++ thank-you1.php
@@ -7,35 +7,35 @@
 <html class="no-js lt-ie9" lang=""> <![endif]-->
 <!--[if gt IE 8]><!-->
 <html class="no-js" lang="">
-<!--<![endif]-->
+    <!--<![endif]-->
 
-<head>
-    <?php include("includes/include_css.html") ?>
-</head>
+    <head>
+        <?php include("includes/include_css.html") ?>
+    </head>
 
-<body>
-    <?php include("includes/browser_upgrade.html") ?>
-    <!--  insert body content  -->
-    <section id="thank-you1" class="thank-you">
-        <div class="container">
-            <div class="banner">
-                <div class="thank_you">
-                    <div class="logo">
-                        <a href="about_us.php">
-                            <img src="dist/img/home/header_main_logo.png" class="img-responsive" alt="" />
-                        </a>
+    <body>
+        <?php include("includes/browser_upgrade.html") ?>
+        <!--  insert body content  -->
+        <section id="thank-you1" class="thank-you">
+            <div class="container">
+                <div class="banner">
+                    <div class="thank_you">
+                        <div class="logo">
+                            <a href="about_us.php">
+                                <img src="dist/img/home/thankyou_logo.png" class="img-responsive" alt="" />
+                            </a>
+                        </div>
+                        <h2>Thank You!</h2>
+                        <h5>We've received your enquiry. Our team will get in touch with you soon.
+                        </h5>
                     </div>
-                    <h2>Thank You!</h2>
-                    <h5>We've received your enquiry. Our team will get in touch with you soon.
-                    </h5>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
 
-    <!--  end body content -->
-    <?php include("includes/include_js.html") ?>
+        <!--  end body content -->
+        <?php include("includes/include_js.html") ?>
 
-</body>
+    </body>
 
 </html>
