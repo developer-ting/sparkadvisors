2ffc51fb6c44118070f001c4effbcf51abf12fef
sagar-cr7 <sagarvb7@gmail.com>
2025-05-05 18:27:23 +0530

    +

--- contact.php
+++ contact.php
@@ -7,80 +7,83 @@
 <html class="no-js lt-ie9" lang=""> <![endif]-->
 <!--[if gt IE 8]><!-->
 <html class="no-js" lang="">
-    <!--<![endif]-->
+<!--<![endif]-->
 
-    <head>
-        <?php include("includes/include_css.html") ?>
-        <?php include("includes/header_1.html") ?>
-    </head>
+<head>
+    <?php include("includes/include_css.html") ?>
+    <?php include("includes/header_1.html") ?>
+</head>
 
-    <body>
-        <?php include("includes/browser_upgrade.html") ?>
-        <!--  insert body content  -->
-        <section id="contact" class="contact">
-            <div class="banner">
-                <div class="banner_text">
-                    <h5>CONNECT WITH US</h5>
-                    <h1>GET IN TOUCH</h1>
-                    <ul>
-                        <li>
-                            <img src="dist/img/regulatory/home.png" class="img-responsive" alt=""><a href="#">HOME
-                                &emsp;></a>
-                        </li>
-                        <li><a href="#">CONTACT US</a></li>
-                    </ul>
-                </div>
+<body>
+    <?php include("includes/browser_upgrade.html") ?>
+    <!--  insert body content  -->
+    <section id="contact" class="contact">
+        <div class="banner">
+            <div class="banner_text">
+                <h5>CONNECT WITH US</h5>
+                <h1>GET IN TOUCH</h1>
+                <ul>
+                    <li>
+                        <img src="dist/img/regulatory/home.png" class="img-responsive" alt=""><a href="#">HOME
+                            &emsp;></a>
+                    </li>
+                    <li><a href="#">CONTACT US</a></li>
+                </ul>
             </div>
-            <div class="contact_form">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-md-12 no_padd">
-                            <div class="contact_form_section">
-                                <h2 class="send_msg">SEND MESSAGE</h2>
-                                <form action="#" method="post" id="contact_page_form" class="contact_page_form">
-                                    <div class="col-md-4">
-                                        <div class="form-group">
-                                            <input type="text" class="form-control input_width" name="name" id="name"
-                                                placeholder="full name">
-                                        </div>
+        </div>
+        <div class="contact_form">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12 no_padd">
+                        <div class="contact_form_section">
+                            <h2 class="send_msg">SEND MESSAGE</h2>
+                            <form action="#" method="post" id="contact_page_form" class="contact_page_form">
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control input_width" name="name" id="name"
+                                            placeholder="full name">
                                     </div>
-                                    <div class="col-md-4">
-                                        <div class="form-group">
-                                            <input type="email" class="form-control input_width" name="user_id"
-                                                id="user_id" placeholder="EMAIL ADDRESS">
-                                        </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        <input type="email" class="form-control input_width" name="email" id="email"
+                                            placeholder="EMAIL ADDRESS">
                                     </div>
-                                    <div class="col-md-4">
-                                        <div class="form-group">
-                                            <input id="subject" type="text" class="form-control input_width"
-                                                name="subject" placeholder="SUBJECT">
-                                            <!--                                            <input type="text" class="form-control" name="new_password" id="new_password" placeholder="PLEASE ENTER NEW PASSWORD">-->
-                                        </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        <input id="subject" type="text" class="form-control input_width" name="subject"
+                                            placeholder="SUBJECT">
+                                        <!--                                            <input type="text" class="form-control" name="new_password" id="new_password" placeholder="PLEASE ENTER NEW PASSWORD">-->
                                     </div>
-                                    <div class="col-md-4">
-                                        <div class="form-group">
-                                            <input id="number" type="number" class="form-control input_width"
-                                                name="number" placeholder="Mobile Number">
-                                            <!--                                            <input type="text" class="form-control" name="new_password" id="new_password" placeholder="PLEASE ENTER NEW PASSWORD">-->
-                                        </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        <input id="number" type="number" class="form-control input_width" name="number"
+                                            placeholder="Mobile Number">
+                                        <!--                                            <input type="text" class="form-control" name="new_password" id="new_password" placeholder="PLEASE ENTER NEW PASSWORD">-->
                                     </div>
-                                    <div class="col-md-12">
-                                        <div class="form-group" colspan="5">
-                                            <textarea class="form-control" rows="4" name="comment"
-                                                placeholder="COMMENT"></textarea>
-                                        </div>
+                                </div>
+                                <div class="col-md-12">
+                                    <div class="form-group" colspan="5">
+                                        <textarea class="form-control" rows="4" name="comment"
+                                            placeholder="COMMENT"></textarea>
                                     </div>
-                                    <div class="col-md-12 button_section">
-                                        <button type="submit" class="log_in_btn">SUBMIT</button>
-                                    </div>
-                                </form>
+                                </div>
+                                <div class="col-md-12 button_section">
+                                    <button type="submit" class="log_in_btn">SUBMIT</button>
+                                </div>
+                            </form>
+                            <div class="thankyou hidden">
+                                thanks
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
 
-            <!-- <div class="container">
+        <!-- <div class="container">
             <div class="button_section">
                 <div class="register_btn">
                     <p>
@@ -89,58 +92,83 @@
                 </div>
             </div>
         </div> -->
-        </section>
+    </section>
 
-        <!--  end body content -->
-        <?php include("includes/include_js.html") ?>
-        <?php include("includes/footer.html") ?>
+    <!--  end body content -->
+    <?php include("includes/include_js.html") ?>
+    <?php include("includes/footer.html") ?>
 
-        <script>
-        $("#contact_page_form").validate({
-            rules: {
-                name: {
-                    required: true,
-                    minlength: 2,
-                    lettersonly: true
-                },
-                user_id: {
-                    required: true,
-                    email: true
-                },
-                subject: {
-                    required: true
-                },
-                number: {
-                    required: true
-                },
-                comment: {
-                    required: true
-                }
-
+    <script>
+    $("#contact_page_form").validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2,
+                lettersonly: true
             },
-            messages: {
-                name: {
-                    required: 'This field is required',
-                    minlength: 'Enter atleast two character',
-                    lettersonly: 'Must enter character only'
-                },
-                user_id: {
-                    required: 'This field is required',
-                    emailtest: " Please enter a valid email address"
-                },
-                subject: {
-                    required: 'This field is required',
-                },
-                number: {
-                    required: 'This field is required',
-                },
-                comment: {
-                    required: 'This field is required',
-                }
-
+            email: {
+                required: true,
+                email: true
+            },
+            subject: {
+                required: true
+            },
+            number: {
+                required: true
+            },
+            comment: {
+                required: true
             }
-        });
-        </script>
-    </body>
+
+        },
+        messages: {
+            name: {
+                required: 'This field is required',
+                minlength: 'Enter atleast two character',
+                lettersonly: 'Must enter character only'
+            },
+            user_id: {
+                required: 'This field is required',
+                emailtest: " Please enter a valid email address"
+            },
+            subject: {
+                required: 'This field is required',
+            },
+            number: {
+                required: 'This field is required',
+            },
+            comment: {
+                required: 'This field is required',
+            }
+
+        }
+        submitHandler: function(form) {
+            $.ajax({
+                url: "https://script.google.com/macros/s/AKfycbzyG_J53KscXM3wqruNAACS1tcHbJoLWw5Hs3E3uGxka-2bA-tZxOCX2ZVHCtCoQ_RJ/exec",
+                type: 'post',
+                data: $("#contact_page_form").serializeArray(),
+                success: function($response) {
+                    $('#thankyou').removeClass('hidden');
+                    // $('body').css({
+                    //     "overflow": "hidden",
+                    //     "position": "relative"
+                    // });
+                    $('#contact_page_form')[0].reset();
+                    setTimeout(function() {
+                        $('#thankyou').addClass('hidden');
+                        // $('body').css({
+                        //     "overflow": "unset",
+                        //     "position": "unset"
+                        // });
+                    }, 5000);
+                },
+                error: function() {
+                    alert("There was an error. PLease try again.")
+                }
+            });
+        }
+    });
+    </script>
+</body>
 
 </html>
