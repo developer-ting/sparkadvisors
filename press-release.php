ad5dad996e6b91f6b5d141cd064a92f2e0b8688f
NikhileshGurrapu13 <nikhilesh.gurrapu@ting.in>
2025-05-27 19:35:52 +0530

    new page added

---
+++ press-release.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>
+<html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>
+<html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>
+<html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="">
+    <!--<![endif]-->
+
+    <head>
+        <?php include("includes/include_css.html") ?>
+        <?php include("includes/header_1.html") ?>
+
+    </head>
+
+    <body>
+        <?php include("includes/browser_upgrade.html") ?>
+        <!--  insert body content  -->
+
+        <section id="press_release" class="press_release">
+            <section class="first_section">
+                <div class="container">
+                    <h4>Spark Capital PWM establishes strategic presence in
+                        Dubai International Financial Centre to serve global investors</h4>
+                    <ul>
+                        <li>
+                            <p>
+                                Spark Capital through its Investment Banking, Asset Management and Private Wealth
+                                Management services, has evolved into a comprehensive financial services institution in
+                                India.
+                            </p>
+                        </li>
+                        <li>
+                            <p>
+                                Spark Global PWM Private Limited, a CAT4 firm regulated by Dubai Financial Services
+                                Authority, the independent regulator for business conducted from or within Dubai
+                                International Financial Centre (DIFC), operates as a subsidiary of Spark Capital’s
+                                Private
+                                Wealth Management business and the new office in will serve as a hub for its Middle
+                                East operations.
+                            </p>
+                        </li>
+                    </ul>
+
+                    <p>
+                        <span>Dubai, UAE: XX May 2025</span> – Spark Capital Private Wealth Management (Spark
+                        Capital PWM), today
+                        announced the launch of its operations in Dubai International Financial Centre (DIFC), the
+                        leading
+                        global financial centre in the Middle East, Africa and South Asia (MEASA) region through Spark
+                        Global
+                        PWM Private Limited.
+                    </p>
+                    <p>
+                        This strategic expansion will enable global investors to access diverse markets with ease while
+                        offering clients comprehensive portfolio diversification opportunities. The move reinforces
+                        Spark
+                        Capital&#39;s commitment to delivering sophisticated, bespoke wealth management solutions on an
+                        international scale.
+                    </p>
+                    <p>
+                        Spark Capital PWM has significantly focused on growth &amp; expansion in recent years. Its AUM
+                        &amp; AUA
+                        has grown nearly 9x—from INR ~3,000 crores (~360.24 million USD) in April 2023 to INR 30,000
+                        crores (~3.53 billion USD) in May 2025. The firm has also scaled its team from 60 to over 400
+                        professionals, including 130+ seasoned relationship managers, and established a pan-India
+                        presence across 12 cities.
+                    </p>
+                    <p>
+                        Commenting on the expansion, <span>Arpita Vinay, Senior Managing Director &amp; Co-CEO, Spark
+                            Capital
+                            Private Wealth Management Private Limited,</span> said, &quot;The establishment of our DIFC
+                        office
+                        represents a significant milestone in Spark Capital PWM&#39;s global growth strategy.
+                        Dubai&#39;s strategic
+                        location and robust financial infrastructure provide an ideal platform to connect investors
+                        across
+                        regions with diverse investment opportunities. This expansion allows us to enhance our service
+                        offerings to international clients while creating new avenues for wealth creation and
+                        preservation.&quot;
+                    </p>
+                    <p>
+                        <span>Neeraj Ojha, Senior Executive Officer, Spark Global PWM Private Limited,</span> added,
+                        &quot;DIFC
+                        offers
+                        an exceptional ecosystem for wealth management firms, with its world-class regulatory framework
+                        and access to a wide network of financial institutions. Through our presence here, we aim to
+                        provide
+                        professional clients in the Middle East with comprehensive wealth solutions backed by Spark
+                        Capital&#39;s expertise and innovation in investment management.&quot;
+                    </p>
+                    <p>
+                        DIFC is a strategic gateway to financial institutions and multinational firms in the region.
+                        DIFC
+                        continues to experience exponential growth in the wealth and asset management sector with 420
+                        wealth and asset management firms and over 75 pure play hedge funds, 48 of which are ‘billion-
+                        dollar club’ organisations, that capitalising on regional opportunities.
+                    </p>
+                    <p>
+                        Spark Global PWM Private Limited&#39;s DIFC office will be staffed by a team of experienced
+                        wealth
+                        management professionals with extensive knowledge of global markets and a proven track record of
+                        delivering exceptional client outcomes. These professionals will leverage the firm&#39;s
+                        comprehensive
+                        research capabilities, innovative investment strategies, and established track record in wealth
+                        management to provide tailored solutions to clients in the region. This approach underscores
+                        Spark
+                        Capital&#39;s dedication to client-centric service while strengthening its position as a trusted
+                        wealth
+                        management partner globally.
+                    </p>
+                    <p>
+                        <span>About Spark Capital Private Wealth Management</span><br />
+                        Spark PWM Pvt Ltd, operates as a subsidiary of Spark Capital Advisors (India) Limited. Aligning
+                        with
+                        Spark Capital’s core values of Knowledge, Integrity, Trust &amp; Transparency, Spark PWM
+                        endeavours to
+                        assist Affluent Families, Business Owners / Promoters, Family Offices, New Age Entrepreneurs,
+                        and
+                        CXOs in navigating the complexities of the financial landscape. The company’s mission is to
+                        seamlessly facilitate the creation, preservation, and transition of wealth across generations
+                        through
+                        its range of products and services.
+                    </p>
+                    <p>
+                        As part of Spark Capital’s ecosystem, Spark Capital PWM extends its offerings beyond traditional
+                        wealth management services to include investment banking and asset management expertise. This
+                        integrated approach enables Spark Capital PWM to tailor bespoke solutions to meet the diverse
+                        needs of its clientele.
+                    </p>
+                    <p>
+                        <span>Media Contacts:</span><br />
+                        Aashish Mudbidri<br />
+                        Group Head- Branding &amp; Corporate Communications<br />
+                        Spark Capital Advisors Pvt. Ltd.<br />
+                        <a href="mailto:Aashish.m@sparkcapital.in" target="_blank"
+                            rel="noopener noreferrer">Aashish.m@sparkcapital.in</a>
+                    </p>
+                    <p>
+                        <span>About Dubai International Financial Centre</span><br />
+                        Dubai International Financial Centre (DIFC) is one of the world’s most advanced
+                        financial centres,
+                        and the leading financial hub for the Middle East, Africa, and South Asia (MEASA), which
+                        comprises
+                        77 countries with an approximate population of 3.7bn and an estimated GDP of USD 10.5trn.
+                    </p>
+                    <p>
+                        With a 20-year track record of facilitating trade and investment flows across the MEASA region,
+                        the
+                        Centre connects these fast-growing markets with the economies of Asia, Europe, and the Americas
+                        through Dubai.
+                    </p>
+                    <p>
+                        DIFC is home to an internationally recognised, independent regulator and a proven judicial
+                        system
+                        with an English common law framework, as well as the region’s largest financial ecosystem of
+                        46,000
+                        professionals working across over 6,900 active registered companies – making up the largest and
+                        most diverse pool of industry talent in the region.
+                    </p>
+                    <p>
+                        The Centre’s vision is to drive the future of finance through cutting-edge technology,
+                        innovation, and
+                        partnerships. Today, it is the global future of finance and innovation hub offering one of the
+                        region’s
+                        most comprehensive AI, FinTech and venture capital environments, including cost-effective
+                        licensing
+                        solutions, fit-for-purpose regulation, innovative accelerator programmes, and funding for
+                        growth-
+                        stage start-ups.
+                    </p>
+                    <p>
+                        Comprising a variety of world-renowned retail and dining venues, a dynamic art and culture
+                        scene,
+                        residential apartments, hotels, and public spaces, DIFC continues to be one of Dubai’s most
+                        sought-
+                        after business and lifestyle destinations.
+                    </p>
+                    <p>
+                        <span>For further information, please visit our website: difc.ae</span><br />
+                        Rasha Mezher | Dubai International Financial Centre Authority<br />
+                        Manager, Marketing &amp; Corporate Communications<br />
+                        <a href="tel:+9714 3622451" target="_blank" rel="noopener noreferrer">+9714 3622451</a><br />
+                        <a href="mailto:Rasha.Mezher@difc.ae" target="_blank"
+                            rel="noopener noreferrer">Rasha.Mezher@difc.ae</a>
+                    </p>
+                </div>
+            </section>
+        </section>
+
+        <!--  end body content -->
+        <?php include("includes/include_js.html") ?>
+        <?php include("includes/footer.html") ?>
+    </body>
+
+</html>
